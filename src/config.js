e8f70207942f46b270fec2c7164aa90d08644c47
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-19 02:29:18 -0500

    Descripción de los cambios realizados

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const BD_DATABASE=process.env.BD_DATABASE|| railway
 export const DB_USER=process.env.DB_USER|| root
 export const DB_PASSWORD=process.env.DB_PASSWORD||'wTLXswmAuuMKHWZGkZGNrkfVHBuYbfuK'
 export const DB_PORT=process.env.DB_PORT|| 3306
-export const PORT= process.env.PORT|| 3000
+export const PORT= process.env.PORT|| 42621
