cc3a1785d1ec75eda7b1e1c72bd3b1ef6f1fd872
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-19 02:25:12 -0500

    Descripción de los cambios realizados

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import {config}from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || localhost
-export const BD_DATABASE=process.env.BD_DATABASE|| base20242
+export const BD_HOST=process.env.BD_HOST || autorack.proxy.rlwy.net
+export const BD_DATABASE=process.env.BD_DATABASE|| railway
 export const DB_USER=process.env.DB_USER|| root
-export const DB_PASSWORD=process.env.DB_PASSWORD||''
+export const DB_PASSWORD=process.env.DB_PASSWORD||'wTLXswmAuuMKHWZGkZGNrkfVHBuYbfuK'
 export const DB_PORT=process.env.DB_PORT|| 3306
 export const PORT= process.env.PORT|| 3000
