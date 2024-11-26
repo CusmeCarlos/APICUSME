2a3d0e55207e5968b94fef8990b85888bce7c5af
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-26 12:00:19 -0500

    Descripción de los cambios realizados

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import {config}from 'dotenv'
 config()
 
 export const BD_HOST=process.env.BD_HOST || autorack.proxy.rlwy.net
-export const BD_DATABASE=process.env.BD_DATABASE|| railway
+export const BD_DATABASE=process.env.BD_DATABASE|| pasantias
 export const DB_USER=process.env.DB_USER|| root
-export const DB_PASSWORD=process.env.DB_PASSWORD||'qbfbXvChoLdCtHxrbKQwWDWIQIPxybPhK'
-export const DB_PORT=process.env.DB_PORT|| 37100
-export const PORT= process.env.PORT|| 37100
+export const DB_PASSWORD=process.env.DB_PASSWORD||'wTLXswmAuuMKHWZGkZGNrkfVHBuYbfuK'
+export const DB_PORT=process.env.DB_PORT|| 42621
+export const PORT= process.env.PORT|| 42621
--- src/db.js
+++ src/db.js
@@ -1,13 +1,22 @@
-import { createPool } from 'mysql2/promise';
-import { BD_HOST, DB_USER, DB_PASSWORD, BD_DATABASE, DB_PORT } from './config.js';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-export const conmysql = createPool({
-  host: BD_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: BD_DATABASE,
-  port: DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+dotenv.config(); // Cargar variables de entorno
+
+const conmysql = mysql.createConnection({
+  host: process.env.BD_HOST,         // 'autorack.proxy.rlwy.net'
+  user: process.env.DB_USER,         // 'root'
+  password: process.env.DB_PASSWORD, // tu contraseña
+  database: process.env.BD_DATABASE, // 'BD_PASANTIAS'
+  port: process.env.DB_PORT          // '37100'
 });
+
+conmysql.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos MySQL en Railway');
+});
+
+export { conmysql };
