47b9d8c33263c2d3aff23b64999665ddc80cd3f2
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-26 13:42:56 -0500

    Descripción de los cambios realizados

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ config()
 export const BD_HOST=process.env.BD_HOST || junction.proxy.rlwy.net
 export const BD_DATABASE=process.env.BD_DATABASE|| railway
 export const DB_USER=process.env.DB_USER|| root
-export const DB_PASSWORD=process.env.DB_PASSWORD||'wTLXswmAuuMKHWZGkZGNrkfVHBuYbfuK'
-export const DB_PORT=process.env.DB_PORT|| 42621
+export const DB_PASSWORD=process.env.DB_PASSWORD||'WMmDrQFwpECSVqheJyJhETMqxNlWRxWQ'
+export const DB_PORT=process.env.DB_PORT|| 47344
 export const PORT= process.env.PORT|| 3000
